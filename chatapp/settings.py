ed70ed4fcbbc1252621b76b54482f961533dc73d
Raza046 <rulmustafa22@gmail.com>
2023-03-08 23:55:34 +0500

    Settings updated for vercel deployment

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -25,14 +25,13 @@ SECRET_KEY = 'django-insecure-b8h+j9bzb_@$-61!cuslh+-!-h(*s%#%eqspzd@wqy$x#t=5ov
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app',"*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'channels',
-    # 'channels_presence',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -128,12 +127,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
